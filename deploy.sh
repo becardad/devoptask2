66e39ea742d4c57b530f86a0bc5caa1386db13fa
becardad <127740181+becardad@users.noreply.github.com>
2025-02-05 16:11:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u sabari22 -p sabari2004
-    docker tag test sabari22/ar
-    docker push sabari22/ar
+    docker tag test sabari22/as
+    docker push sabari22/as
     
